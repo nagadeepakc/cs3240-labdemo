291b62cdb73ba5307f1eecb07e6b4304d49dae49
Naga Deepak Chelluri <nagadeepakc@gmail.com>
2015-09-28 14:28:33 -0400

    implemented a simple adder

---
+++ adder.py
@@ -0,0 +1,2 @@
+def add(x, y):
+	print("sum of " + str(x) + " and " + str(y) + " is " + str(x+y))
--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
 import helper
+import adder
 
 if __name__ == "__main__":
 	# print("hello")
 	helper.greeting("hello2")
+	adder.add(1, 2)
